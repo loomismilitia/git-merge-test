ba062a0c4378e5a9fbe3281fa4e35a83e0d23f54
loomismilitia <simaopqs@gmail.com>
2016-03-20 09:22:37 +0000

    Adding Person class.

---
+++ Person.java
@@ -0,0 +1,14 @@
+public class Person {
+
+    private String firstName;
+    private String lastName;    
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+	
+}
