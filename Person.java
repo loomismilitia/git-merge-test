f037f91937c91c36c75ad6e230a3698361078601
loomismilitia <simaopqs@gmail.com>
2016-03-20 09:32:15 +0000

    Adding validation to the Person class

--- Person.java
+++ Person.java
@@ -2,12 +2,23 @@ public class Person {
 
     private String firstName;
     private String lastName;    
+	
+	public Person(String firstName, String lastName){
+		this.setFirstName(firstName);
+		this.setLastName(lastName);
+	}
 
-    public void setFirstName(String firstName) {
+    public void setFirstName(String firstName) throws IllegalArgumentException {
+		if(firstName.isEmpty()) {
+			throw new IllegalArgumentException("First name can't be empty");
+		}
         this.firstName = firstName;
     }
 
-    public void setLastName(String lastName) {
+    public void setLastName(String lastName) throws IllegalArgumentException {
+		if(lastName.isEmpty()) {
+			throw new IllegalArgumentException("Last name can't be empty");
+		}
         this.lastName = lastName;
     }
 	
